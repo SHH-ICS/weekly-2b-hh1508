1b8d7dc3d52f611365a6841947b20dbd75b095c5
hh1508 <144179073+hh1508@users.noreply.github.com>
2023-10-22 16:45:55 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,16 @@
+import math:
 
+#variables
+int diameter
+int area
+int circumference
+int radius
+int pi = math.pi
+
+diameter = int(input("Please enter the diameter))
+radius = (diameter / 2)
+area  = (pi * (radius ^ 2))
+
+circumference = (2 * (pi * radius))
+
+print(area, circumference)
